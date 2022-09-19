d228b80c9fe1881c54d73d4ebe1bfdb7bfe2c901
Hasnain Raza <Hassanain.okz110@gmail.com>
2022-09-19 16:25:25 +0500

    Second commit

--- lib/PostScreen.dart
+++ lib/PostScreen.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
 import 'AddPost.dart';
@@ -12,6 +13,7 @@ class PostScreen extends StatefulWidget {
 
 class _PostScreenState extends State<PostScreen> {
   final ref = FirebaseDatabase.instance.ref("Post");
+  TextEditingController SearchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -30,34 +32,54 @@ class _PostScreenState extends State<PostScreen> {
       ),
       body: Column(
         children: [
-          // Expanded(
-          //   child: FirebaseAnimatedList(
-          //       query: ref,
-          //       defaultChild: const Text("Loading"),
-          //       itemBuilder: (context, snapshot, animation, index) {
-          //         return ListTile(
-          //           title: Text(snapshot.child("Title").value.toString()),
-          //           subtitle: Text(snapshot.child("id").value.toString()),
-          //         );
-          //       }),
-          // ),
-          TextFormField(
-            decoration: const InputDecoration(
-                hintText: "Search", border: OutlineInputBorder()),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextFormField(
+              onChanged: (value) {},
+              controller: SearchController,
+              decoration: const InputDecoration(
+                  hintText: "Search",
+                  suffixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder()),
+            ),
           ),
           Expanded(
-            child: ListView.builder(
-              itemCount: 20,
-              shrinkWrap: true,
-              primary: false,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(ref.child("Title").toString()),
-                  subtitle: Text(ref.child("id").toString()),
-                );
-              },
-            ),
-          )
+            child: FirebaseAnimatedList(
+                query: ref,
+                defaultChild: const Text("Loading"),
+                itemBuilder: (context, snapshot, animation, index) {
+                  final title = snapshot.child("Title").value.toString();
+
+                  if (SearchController.text.isEmpty) {
+                    return ListTile(
+                      title: Text(snapshot.child("Title").value.toString()),
+                      subtitle: Text(snapshot.child("id").value.toString()),
+                    );
+                  } else if (title.toLowerCase().contains(
+                      SearchController.text.toLowerCase().toLowerCase())) {
+                    return ListTile(
+                      title: Text(snapshot.child("Title").value.toString()),
+                      subtitle: Text(snapshot.child("id").value.toString()),
+                    );
+                  } else {
+                    Container();
+                  }
+                  return Container();
+                }),
+          ),
+          // Expanded(
+          //   child: ListView.builder(
+          //     itemCount: 20,
+          //     shrinkWrap: true,
+          //     primary: false,
+          //     itemBuilder: (context, index) {
+          //       return ListTile(
+          //         title: Text(ref.child("Title").toString()),
+          //         subtitle: Text(ref.child("id").toString()),
+          //       );
+          //     },
+          //   ),
+          // )
         ],
       ),
     );
